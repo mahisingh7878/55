69ccd60f4f748e55677a9c314833a92c549860a8
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2025-01-04 20:50:30 +0530

    actvitya.2py

---
+++ actvityya2.py
@@ -0,0 +1,14 @@
+def fun1(n):
+    return n*(n+1)/2
+
+
+print("sum of numbers  is ",fun1(4))
+
+def fun2(n):
+    sum=0
+    for i in range(1,n+1):
+        sum+=i
+    return sum
+    
+print("sum of the number is ",fun2(4))
+
